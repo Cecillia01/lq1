dbca2604cc46b37e3ae6b4f60e5326161f542bfd
labquiz1 <botio.mariacecillia@gmail.com>
2024-09-10 21:27:30 +0800

    Added Division Operation

--- calcu.java
+++ calcu.java
@@ -8,4 +8,10 @@ public int subtract(int a, int b) {
 public int multiply(int a, int b) {
         return a * b;
     }
+ public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
 }
